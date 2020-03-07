7c3992e966199cd8bcee3db3deeac51bd8fb377c
Alameen688 <ogundiran12@gmail.com>
2020-03-07 23:55:57 +0100

    Add first gsap animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { TweenMax, Power3 } from "gsap";
 
 function App() {
+  let logoItem = useRef(null);
+  let textItem = useRef(null);
+
+  console.log(logoItem);
+  useEffect(() => {
+    console.log(logoItem);
+    TweenMax.to(logoItem, 0.8, { opacity: 1, y: -20, ease: Power3.easeOut });
+    TweenMax.to(textItem, 0.8, {
+      opacity: 1,
+      y: -20,
+      ease: Power3.easeOut,
+      delay: 0.2
+    });
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <img
+          ref={el => {
+            logoItem = el;
+          }}
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p
+          ref={el => {
+            textItem = el;
+          }}
+          className="App-text"
+        >
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
