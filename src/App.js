f0e1f9eea33858e3a030580a5d4356527e481214
Alameen688 <ogundiran12@gmail.com>
2020-03-08 16:37:57 +0100

    Add animated dots effect [2]

--- src/App.js
+++ src/App.js
@@ -1,50 +1,75 @@
-import React, { useRef, useEffect } from "react";
-import logo from "./logo.svg";
+import React, { useRef, useEffect, useState } from "react";
 import "./App.css";
 import { TweenMax, Power3 } from "gsap";
 
 function App() {
-  let logoItem = useRef(null);
-  let textItem = useRef(null);
+  let app = useRef(null);
 
-  console.log(logoItem);
+  let circleYellow = useRef(null);
+  let circleRed = useRef(null);
+  let circleBlue = useRef(null);
+
+  const [selectedCircle, setSelectedCircle] = useState(false);
+
+  const handleCircleClick = () => {
+    if (selectedCircle) {
+      TweenMax.to(circleRed, 0.8, {
+        width: 75,
+        height: 75,
+        ease: Power3.easeOut
+      });
+    } else {
+      TweenMax.to(circleRed, 0.8, {
+        width: 200,
+        height: 200,
+        ease: Power3.easeOut
+      });
+    }
+    setSelectedCircle(!selectedCircle);
+  };
+
   useEffect(() => {
-    console.log(logoItem);
-    TweenMax.to(logoItem, 0.8, { opacity: 1, y: -20, ease: Power3.easeOut });
-    TweenMax.to(textItem, 0.8, {
-      opacity: 1,
-      y: -20,
-      ease: Power3.easeOut,
-      delay: 0.2
-    });
+    TweenMax.to(app, 0, { css: { visibility: "visible" } });
+    TweenMax.staggerFrom(
+      [circleYellow, circleRed, circleBlue],
+      0.8,
+      {
+        opacity: 0,
+        x: 40,
+        ease: Power3.easeOut
+      },
+      0.2
+    );
   }, []);
   return (
-    <div className="App">
+    <div
+      ref={el => {
+        app = el;
+      }}
+      className="App"
+    >
       <header className="App-header">
-        <img
-          ref={el => {
-            logoItem = el;
-          }}
-          src={logo}
-          className="App-logo"
-          alt="logo"
-        />
-        <p
-          ref={el => {
-            textItem = el;
-          }}
-          className="App-text"
-        >
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="circle-container">
+          <div
+            ref={el => {
+              circleYellow = el;
+            }}
+            className="circle yellow"
+          ></div>
+          <div
+            ref={el => {
+              circleRed = el;
+            }}
+            className="circle red"
+            onClick={handleCircleClick}
+          ></div>
+          <div
+            ref={el => {
+              circleBlue = el;
+            }}
+            className="circle blue"
+          ></div>
+        </div>
       </header>
     </div>
   );
