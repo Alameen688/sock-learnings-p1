66903cdf9a0ce51aea18412e5ba601fd4518672c
Alameen688 <ogundiran12@gmail.com>
2020-03-09 22:03:11 +0100

    Add hero images and text content animation

--- src/App.js
+++ src/App.js
@@ -1,170 +1,152 @@
 import "reset-css";
 import "./App.scss";
-import leftArrow from "./assets/arrow-left.svg";
-import rightArrow from "./assets/arrow-right.svg";
+import ArrowRight from "./assets/arrow-right.svg";
+import ImgBoy from "./assets/boy.webp";
+import ImgGirl from "./assets/girl.webp";
 import React, { useEffect, useRef, useState } from "react";
-import { TweenLite, Power3 } from "gsap";
-
-const testimonials = [
-  {
-    name: "Julia Cameron",
-    title: "Creative Director, VISA",
-    image: `${require("./assets/image3.jpg")}`,
-    quote:
-      "It's all good. I was amazed at the quality of the Design. We've seen amazing results already."
-  },
-  {
-    name: "Mark Jacobs",
-    title: "Tech Lead, Google",
-    image: `${require("./assets/image.jpg")}`,
-    quote:
-      "The rebranding has really helped our business. Definitely worth the investment."
-  },
-  {
-    name: "Lisa Bearings",
-    title: "Brand Coordinator, Facebook",
-    image: `${require("./assets/image2.jpg")}`,
-    quote:
-      "The service was excellent. Absolutely wonderful! A complete redesign did it for us."
-  }
-];
+import { TimelineLite, TweenMax, Power3 } from "gsap";
 
 function App() {
-  const IMAGE_WIDTH = 340;
-  let imageList = useRef(null);
-  let testimonialList = useRef(null);
+  let app = useRef(null);
+  let images = useRef(null);
 
-  const [state, setState] = useState({
-    isActive: 0
-  });
+  let contents = useRef(null);
+
+  // DELAY ENTIRE TIMELINELITE by .8, making sure the page is visible and ready by then
+  const tl = new TimelineLite({ delay: 0.8 });
 
   useEffect(() => {
-    TweenLite.to(testimonialList.children[0], 0, { opacity: 1 });
-  }, []);
+    const girlImage = images.firstElementChild;
+    const boyImage = images.lastElementChild;
 
-  const slideLeft = (index, multiplied = 1, duration = 1) => {
-    TweenLite.to(imageList.children[index], duration, {
-      x: -IMAGE_WIDTH * multiplied,
-      ease: Power3.easeOut
-    });
-  };
+    const headlineFirst = contents.children[0].children[0];
+    const headlineSecond = headlineFirst.nextSibling;
+    const headlineThird = headlineSecond.nextSibling;
 
-  const slideRight = (index, multiplied = 1, duration = 1) => {
-    TweenLite.to(imageList.children[index], duration, {
-      x: IMAGE_WIDTH * multiplied,
-      ease: Power3.easeOut
-    });
-  };
+    const contentP = contents.children[1];
+    const contentButton = contents.children[2];
 
-  const scale = (index, duration = 1) => {
-    TweenLite.from(imageList.children[index], duration, {
-      scale: 1.2,
-      ease: Power3.easeOut
-    });
-  };
+    //show entire page section
+    TweenMax.to(app, 0, { visibility: "visible" });
 
-  const fadeOut = (index, duration = 1) => {
-    TweenLite.to(testimonialList.children[index], duration, {
-      opacity: 0
-    });
-  };
-  const fadeIn = (index, duration = 1) => {
-    TweenLite.to(testimonialList.children[index], duration, {
-      opacity: 1,
-      delay: 1
-    });
-  };
+    // IMAGES ANIMATION Timeline
+    // delay of .2 in a timeline overides how long it should (default is after the prev anim is done) wait before it proceeds to the next anim in the timeline
+    // all timelines with the same tag of "set 1" will start at the same time
+    tl.from(
+      girlImage,
+      1.2,
+      {
+        y: 1200,
+        ease: Power3.easeOut
+      },
+      "set 1"
+    )
+      .from(
+        girlImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      )
+      .from(
+        boyImage,
+        1.2,
+        {
+          y: 1200,
+          ease: Power3.easeOut
+        },
+        0.2
+      )
+      .from(
+        boyImage.firstElementChild,
+        2,
+        { scale: 1.6, ease: Power3.easeOut },
+        0.2
+      );
 
-  const handlePrevSlide = () => {
-    if (state.isActive > 0) {
-      for (let i = state.isActive + 1; i > 0; i--) {
-        console.log("loop start", i);
-        console.log(state.isActive);
-        console.log("ans", 1 - state.isActive);
-        slideRight(i, 1 - state.isActive || 1);
-        console.log("loop end");
-      }
-      // slideRight(state.isActive, 0);
-      // slideRight(state.isActive - 1, 0);
-
-      // slideRight(state.isActive, -1);
-      // slideRight(state.isActive - 1, -1);
-
-      // slideRight(state.isActive - 1, 0);
-      // slideRight(state.isActive - 2, 0);
-      scale(state.isActive - 1);
-
-      // fadeOut(state.isActive);
-      // fadeIn(state.isActive + 1);
-
-      setState(prevState => ({
-        isActive: prevState.isActive - 1
-      }));
-    } else {
-      setState({
-        isActive: imageList.children.length - 1
-      });
-    }
-  };
-
-  const handleNextSlide = () => {
-    if (state.isActive < imageList.children.length - 1) {
-      for (let i = state.isActive; i < imageList.children.length; i++) {
-        slideLeft(i, state.isActive + 1);
-      }
-      scale(state.isActive + 1);
-
-      fadeOut(state.isActive);
-      fadeIn(state.isActive + 1);
-
-      setState(prevState => ({
-        isActive: prevState.isActive + 1
-      }));
-    } else {
-      setState({
-        isActive: 0
-      });
-    }
-  };
+    //CONTENT ANIMATION Timeline
+    tl.staggerFrom(
+      [headlineFirst.children, headlineSecond.children, headlineThird.children],
+      1,
+      {
+        y: 44,
+        ease: Power3.easeIn,
+        delay: 0.8
+      },
+      0.15,
+      "set 1"
+    )
+      .from(contentP, 1, { y: 20, opacity: 0, ease: Power3.easeInOut }, 1.4)
+      .from(
+        contentButton,
+        1,
+        { y: 20, opacity: 0, ease: Power3.easeInOut },
+        1.6
+      );
+  }, [tl]);
 
   return (
-    <section className="testimonial-section">
-      <div className="testimonial-container">
-        <div onClick={handlePrevSlide} className="arrows left">
-          <span>
-            <img src={leftArrow} alt="arrow left" />
-          </span>
-        </div>
-
-        <div className="inner">
-          <div className="t-image">
-            <ul ref={el => (imageList = el)}>
-              {testimonials.map((testimonial, i) => (
-                <li className={state.isActive == i ? "active" : null}>
-                  <img src={testimonial.image} alt={testimonial.name} />
-                </li>
-              ))}
-            </ul>
+    <section
+      ref={el => {
+        app = el;
+      }}
+      className="hero"
+    >
+      <div className="container">
+        <div className="hero-inner">
+          <div className="hero-content">
+            <div
+              ref={el => {
+                contents = el;
+              }}
+              className="hero-content-inner"
+            >
+              <h1>
+                <span className="hero-content-line">
+                  <span className="hero-content-line-inner">
+                    Relieving the burden
+                  </span>
+                </span>
+                <span className="hero-content-line">
+                  <span className="hero-content-line-inner">
+                    of disease caused
+                  </span>
+                </span>
+                <span className="hero-content-line">
+                  <span className="hero-content-line-inner">
+                    by behaviours.
+                  </span>
+                </span>
+              </h1>
+              <p>
+                Better treats serious cardiometabolic diseases to transform
+                lives and reduce healthcare ustilisation through the use of
+                digital therapeutics
+              </p>
+              <div className="btn-row">
+                <button className="explore-button">
+                  explore
+                  <span className="arrow-icon">
+                    <img src={ArrowRight} alt="arrow" />
+                  </span>
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="t-content">
-            <ul ref={el => (testimonialList = el)}>
-              {testimonials.map((testimonial, i) => (
-                <li className={state.isActive == i ? "active" : null}>
-                  <div className="content-inner">
-                    <p className="quote">{testimonial.quote}</p>
-                    <h3 className="name">{testimonial.name}</h3>
-                    <h4 className="title">{testimonial.title}</h4>
-                  </div>
-                </li>
-              ))}
-            </ul>
+          <div className="hero-images">
+            <div
+              ref={el => {
+                images = el;
+              }}
+              className="hero-images-inner"
+            >
+              <div className="hero-image girl">
+                <img src={ImgGirl} alt="girl image" />
+              </div>
+              <div className="hero-image boy">
+                <img src={ImgBoy} alt="boy image" />
+              </div>
+            </div>
           </div>
-        </div>
-
-        <div onClick={handleNextSlide} className="arrows right">
-          <span>
-            <img src={rightArrow} alt="arrow right" />
-          </span>
         </div>
       </div>
     </section>
